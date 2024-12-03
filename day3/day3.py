e412959fe7744daa3d6913e50298643fb7bee325
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-03 21:52:15 +0100

    day 3, task 2

--- day3/day3.py
+++ day3/day3.py
@@ -24,10 +24,27 @@ def process(input):
     return result
 
 
+def conditional_process(input):
+    result = 0
+
+    # partition input into 'do()' blocks first:
+    parts = input.split('do()')
+
+    for part in parts:
+        # then split each part,
+        # to ignore everything to the right of 'don't()' keyword:
+        only_good_stuff = part.split("don't()")[0]
+        result += process(only_good_stuff)
+
+    return result
+
+
 def main():
     input = read_input()
     result = process(input)
+    result_with_conditions = conditional_process(input)
     print(f'Result: {result}')
+    print(f'Result with extra conditions: {result_with_conditions}')
 
 
 if __name__ == "__main__":
--- day3/day3_test.py
+++ day3/day3_test.py
@@ -1,4 +1,4 @@
-from day3 import process
+from day3 import process, conditional_process
 
 def test_process():
     input = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
@@ -6,3 +6,9 @@ def test_process():
     result = process(input)
     assert result == 161
 
+
+def test_conditional_process():
+    input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+    result = conditional_process(input)
+    assert result == 48
