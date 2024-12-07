756a7b60e6f7f30b9e874e4b8c0a7d4907d0c28d
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-07 14:41:38 +0100

    day 7, task 2

--- day7/solution.py
+++ day7/solution.py
@@ -31,18 +31,37 @@ def true_equation(equation: List) -> bool:
     return test(0, operands[0])
 
 
-def sum_true_equations(equations: List) -> int:
+def true_equation_with_concat(equation: List) -> bool:
+    result, operands = equation
+
+    def test(curr_index: int, curr_res: int) -> bool:
+        if curr_index >= len(operands) - 1:
+            return curr_res == result
+        else:
+            nxt_index = curr_index + 1
+            return (
+                test(nxt_index, curr_res * operands[nxt_index])
+                or test(nxt_index, curr_res + operands[nxt_index])
+                or test(nxt_index, int(f"{curr_res}{operands[nxt_index]}"))
+            )
+
+    return test(0, operands[0])
+
+
+def sum_true_equations(equations: List, test_func) -> int:
     res = 0
     for eq in equations:
-        if true_equation(eq):
+        if test_func(eq):
             res += eq[0]
     return res
 
 
 def main():
     equations = read_input()
-    result = sum_true_equations(equations)
-    print(f"Sum of true equations results: {result}")
+    result = sum_true_equations(equations, true_equation)
+    result_with_concat = sum_true_equations(equations, true_equation_with_concat)
+    print(f"Sum of true equations: {result}")
+    print(f"Sum of true equations with concat: {result_with_concat}")
 
 
 if __name__ == "__main__":
