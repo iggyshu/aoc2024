dc0d231d1dbe3797b75f4ae15c5c4d4c3cd5439f
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-08 14:11:57 +0100

    day 8, task 2

--- day8/solution.py
+++ day8/solution.py
@@ -60,10 +60,59 @@ def count_antinodes(grid: List[List[str]]) -> int:
     return len(antinodes)
 
 
+def spawn_antinode(
+    grid: List[List[str]], node: (int, int), row_inc: int, col_inc: int, antinodes: set
+):
+    if in_bounds(grid, node):
+        antinodes.add(node)
+        row, col = node
+
+        spawn_antinode(
+            grid, (row + row_inc, col + col_inc), row_inc, col_inc, antinodes
+        )
+
+
+def with_resonant_harmonics(grid: List[List[str]]) -> int:
+    nodes_by_value = defaultdict(list)  # {'a': [(0, 1), ('2, 4')]}
+    antinodes = set()  # {(10, 11), (5, 6)}
+
+    M, N = len(grid), len(grid[0])
+
+    for row in range(M):
+        for col in range(N):
+            value = grid[row][col]
+            if value == ".":
+                continue
+            if value in nodes_by_value:
+                for node in nodes_by_value[value]:
+                    row_1, col_1 = node
+                    row_dist = abs(row_1 - row)
+                    col_dist = abs(col_1 - col)
+
+                    an_row_inc_1, an_col_inc_1 = row_dist, col_dist
+                    an_row_inc_2, an_col_inc_2 = -row_dist, -col_dist
+                    if col < col_1:
+                        # edge case when current node is to the left
+                        # of previously encountered node
+                        an_col_inc_1 = -col_dist
+                        an_col_inc_2 = col_dist
+
+                    spawn_antinode(
+                        grid, (row, col), an_row_inc_1, an_col_inc_1, antinodes
+                    )
+                    spawn_antinode(grid, node, an_row_inc_2, an_col_inc_2, antinodes)
+
+            nodes_by_value[value].append((row, col))
+
+    return len(antinodes)
+
+
 def main():
     grid = read_grid()
     count = count_antinodes(grid)
     print(f"Found {count} antinodes.")
+    count_with_harmonics = with_resonant_harmonics(grid)
+    print(f"Found {count_with_harmonics} antinodes using updated model with harmonics.")
 
 
 if __name__ == "__main__":
--- day8/solution_test.py
+++ day8/solution_test.py
@@ -1,4 +1,4 @@
-from solution import read_grid, print_grid, count_antinodes
+from solution import read_grid, print_grid, count_antinodes, with_resonant_harmonics
 
 
 def test_read_input():
@@ -13,3 +13,9 @@ def test_count_antinodes():
     grid = read_grid("test_input.txt")
     count = count_antinodes(grid)
     assert count == 14
+
+
+def test_with_resonant_harmonics():
+    grid = read_grid("test_input.txt")
+    count = with_resonant_harmonics(grid)
+    assert count == 34
