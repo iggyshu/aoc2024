723eccbe3e05aaaacbf64aa76a1b1c0aa19c44ba
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-10 22:19:29 +0100

    day 10, task 1

--- day10/solution.py
+++ day10/solution.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple
-from collections import defaultdict
+from collections import deque
 
 
 def read_grid(filename: str = "input.txt") -> List[List[str]]:
@@ -25,8 +25,64 @@ def find_trailheads(grid: List[List[str]]) -> List[Tuple[int, int]]:
     return trailheads
 
 
+def trailhead_score(grid: List[List[str]], trailhead: Tuple[int, int]) -> int:
+    def in_bounds(node):
+        row, col = node
+        return row >= 0 and col >= 0 and row < len(grid) and col < len(grid[0])
+
+    def is_gradual(node, next_node):
+        return int(grid[next_node[0]][next_node[1]]) - int(grid[node[0]][node[1]]) == 1
+
+    def is_peak(node):
+        return grid[node[0]][node[1]] == "9"
+
+    visited = set()
+    score = 0
+
+    frontier = deque()
+    frontier.append(trailhead)
+
+    print("")
+    print("beginning BFS")
+
+    while frontier:
+        print(frontier)
+        node = frontier.popleft()
+        visited.add(node)
+
+        if is_peak(node):
+            score += 1
+        else:
+            up = (node[0] - 1, node[1])
+            left = (node[0], node[1] - 1)
+            right = (node[0], node[1] + 1)
+            down = (node[0] + 1, node[1])
+            neighbor_nodes = [up, left, right, down]
+
+            for neighbor in neighbor_nodes:
+                if (
+                    in_bounds(neighbor)
+                    and (neighbor not in visited)
+                    and is_gradual(node, neighbor)
+                    and (neighbor not in frontier)
+                ):
+                    frontier.append(neighbor)
+
+    print(f"trailhead: {trailhead}, score: {score}")
+    return score
+
+
+def trailhead_scores(
+    grid: List[List[str]], trailheads: List[Tuple[int, int]]
+) -> List[int]:
+    return [trailhead_score(grid, trailhead) for trailhead in trailheads]
+
+
 def main():
     grid = read_grid()
+    trailheads = find_trailheads(grid)
+    scores = trailhead_scores(grid, trailheads)
+    print(sum(scores))
 
 
 if __name__ == "__main__":
--- day10/solution_test.py
+++ day10/solution_test.py
@@ -1,4 +1,4 @@
-from solution import read_grid, print_grid, find_trailheads
+from solution import read_grid, print_grid, find_trailheads, trailhead_scores
 
 
 def test_read_input():
@@ -15,3 +15,13 @@ def test_find_trailheads():
     trailheads = find_trailheads(grid)
     print(trailheads)
     assert len(trailheads) == 9
+
+
+def test_trailhead_scores():
+    grid = read_grid("test_input.txt")
+    trailheads = find_trailheads(grid)
+    scores = trailhead_scores(grid, trailheads)
+    print(scores)
+    assert len(scores) == 9
+    assert sorted(scores) == sorted([5, 6, 5, 3, 1, 3, 5, 3, 5])
+    assert sum(scores) == 36
