3ffaa696b5cf59aa28e81325774be9b9b58b2a19
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-11 23:59:35 +0100

    add @cache

--- day11/solution.py
+++ day11/solution.py
@@ -1,4 +1,5 @@
 from typing import List
+from functools import cache
 
 
 def read_input(filename: str = "input.txt") -> List[int]:
@@ -7,25 +8,28 @@ def read_input(filename: str = "input.txt") -> List[int]:
         return [int(x) for x in lines[0].rstrip("\n").split(" ")]
 
 
+@cache
+def process_stone(value: int) -> List[int]:
+    if value == 0:
+        return [1]
+    elif len(str(value)) % 2 == 0:
+        length = len(str(value))
+        divisor = 10 ** (length // 2)
+        left = value // divisor
+        right = value % divisor
+        return [left, right]
+    else:
+        return [value * 2024]
+
+
 def blink(stones: List[int], times: int) -> List[int]:
-    S = list(stones)
-
+    S = stones
     for i in range(times):
+        print(f"blink: {i}, stones: {len(S)}")
         new_stones = []
-        for j in range(len(S)):
-            if S[j] == 0:
-                S[j] = 1
-            elif len(str(S[j])) % 2 == 0:
-                left = right = str(S[j])
-                left = int(left[: len(left) // 2])
-                right = int(right[len(right) // 2 :])
-                S[j] = left
-                new_stones.append(right)
-            else:
-                S[j] *= 2024
-        for ns in new_stones:
-            S.append(ns)
-
+        for value in S:
+            new_stones.extend(process_stone(value))
+        S = new_stones
     return S
 
 
