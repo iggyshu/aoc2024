eee3339d80cbc0fc2ba47e5ae7f7a33f0ac48d1d
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-13 21:41:07 +0100

    day 13, task 1

--- day13/solution.py
+++ day13/solution.py
@@ -1,7 +1,3 @@
-import numpy as np
-from typing import List
-
-
 def parse_params(line: str) -> (int, int):
     right_side = line.split(": ")[-1]
     params = right_side.split(", ")
@@ -14,7 +10,7 @@ def parse_res(line: str) -> (int, int):
     return int(params[0].split("=")[-1]), int(params[1].split("=")[-1])
 
 
-def read_equations(filename: str = "input.txt") -> List[List[str]]:
+def read_equations(filename: str = "input.txt"):
     equations = []
 
     with open(filename) as f:
@@ -33,8 +29,45 @@ def read_equations(filename: str = "input.txt") -> List[List[str]]:
     return equations
 
 
+def solve_equation_brut(equation):
+    X = equation[1][0]
+    Y = equation[1][1]
+    A1 = equation[0][0][0]
+    A2 = equation[0][1][0]
+    B1 = equation[0][0][1]
+    B2 = equation[0][1][1]
+
+    can_solve = False
+    sol = ()
+
+    for i in range(100):
+        for j in range(100):
+            if A1 * i + B1 * j == X and A2 * i + B2 * j == Y:
+                can_solve = True
+                sol = (i, j)
+                break
+
+    return can_solve, sol
+
+
+def solution(filename="input.txt"):
+    equations = read_equations(filename)
+
+    res = 0
+
+    for equation in equations:
+        can_solve, sol = solve_equation_brut(equation)
+        if can_solve:
+            A, B = sol
+
+            cost = A * 3 + B
+            res += cost
+
+    return res
+
+
 def main():
-    pass
+    print(solution())
 
 
 if __name__ == "__main__":
--- day13/solution_test.py
+++ day13/solution_test.py
@@ -1,7 +1,29 @@
-from solution import read_equations
+from solution import read_equations, solution, solve_equation_brut
 
 
 def test_read_input():
     equations = read_equations("test_input.txt")
     print(equations)
     assert (len(equations)) == 4
+
+
+def test_solve_equation():
+    equations = read_equations("test_input.txt")
+    can_solve, sol = solve_equation_brut(equations[0])
+    assert can_solve
+    print(sol)
+    assert tuple(sol) == (80, 40)
+
+    can_solve, sol = solve_equation_brut(equations[1])
+    assert not can_solve
+    can_solve, sol = solve_equation_brut(equations[3])
+    assert not can_solve
+
+    can_solve, sol = solve_equation_brut(equations[2])
+    assert can_solve
+    assert tuple(sol) == (38, 86)
+
+
+def test_solution():
+    res = solution("test_input.txt")
+    assert res == 480
