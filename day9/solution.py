2efaf83fd34a4147f53edcc544892be5f8c10934
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-14 22:55:43 +0100

    day 9, task 2

--- day9/solution.py
+++ day9/solution.py
@@ -42,11 +42,84 @@ def compress(blocks: List[str]) -> List[str]:
     return D
 
 
+def find_spaces(D):
+    def is_empty(index):
+        return D[index] == "."
+
+    spaces = []
+    left = 0
+    while left < len(D):
+        while left < len(D) and not is_empty(left):
+            left += 1
+
+        if left >= len(D):
+            break
+
+        temp = left
+        while temp < len(D) and is_empty(temp):
+            temp += 1
+
+        spaces.append((left, temp))
+
+        left = temp
+
+    return spaces
+
+
+def find_files(D):
+    files = []
+    left = 0
+    while left < len(D):
+
+        def is_empty(index):
+            return D[index] == "."
+
+        while left < len(D) and is_empty(left):
+            left += 1
+
+        temp = left
+        while temp < len(D) and D[temp] == D[left]:
+            temp += 1
+
+        files.append((left, temp))
+
+        left = temp
+
+    return files
+
+
+def compress_full_files(blocks: List[str]) -> List[str]:
+    def size(chunk):
+        return chunk[-1] - chunk[0]
+
+    D = list(blocks)
+
+    spaces = find_spaces(D)
+    files_reversed = find_files(D)[::-1]
+
+    for i, file in enumerate(files_reversed):
+        for j, space in enumerate(spaces):
+            if size(file) <= size(space) and file[0] > space[0]:
+                left = space[0]
+                right = file[0]
+                while right < file[1]:
+                    D[left], D[right] = D[right], D[left]
+                    left += 1
+                    right += 1
+                if size(file) == size(space):
+                    del spaces[j]
+                else:
+                    spaces[j] = left, space[1]
+                break
+
+    return D
+
+
 def checksum(compressed_blocks: List[str]) -> int:
     res = 0
     for id, value in enumerate(compressed_blocks):
         if value == ".":
-            break
+            continue
         res += id * int(value)
     return res
 
@@ -54,9 +127,12 @@ def checksum(compressed_blocks: List[str]) -> int:
 def main():
     diskmap = read_diskmap()
     blocks = diskmap_to_blocks(diskmap)
-    compressed_blocks = compress(blocks)
-    chk = checksum(compressed_blocks)
-    print(f"Checksum: {chk}")
+    # compressed_blocks = compress(blocks)
+    # chk = checksum(compressed_blocks)
+    # print(f"Checksum: {chk}")
+    fullfile_compression = compress_full_files(blocks)
+    chk_files = checksum(fullfile_compression)
+    print(f"Full file checksum: {chk_files}")
 
 
 if __name__ == "__main__":
--- day9/solution_test.py
+++ day9/solution_test.py
@@ -1,4 +1,12 @@
-from solution import read_diskmap, diskmap_to_blocks, compress, checksum
+from solution import (
+    read_diskmap,
+    diskmap_to_blocks,
+    compress,
+    checksum,
+    compress_full_files,
+    find_spaces,
+    find_files,
+)
 
 
 def test_read_diskmap():
@@ -25,3 +33,47 @@ def test_checksum():
     blocks = diskmap_to_blocks(diskmap)
     compressed_blocks = compress(blocks)
     assert checksum(compressed_blocks) == 1928
+
+
+def test_find_spaces():
+    diskmap = read_diskmap("test_input.txt")
+    blocks = diskmap_to_blocks(diskmap)
+    spaces = find_spaces(blocks)
+    assert spaces == [
+        (2, 5),
+        (8, 11),
+        (12, 15),
+        (18, 19),
+        (21, 22),
+        (26, 27),
+        (31, 32),
+        (35, 36),
+    ]
+
+
+def test_find_files():
+    diskmap = read_diskmap("test_input.txt")
+    blocks = diskmap_to_blocks(diskmap)
+    spaces = find_files(blocks)
+    assert spaces == [
+        (0, 2),
+        (5, 8),
+        (11, 12),
+        (15, 18),
+        (19, 21),
+        (22, 26),
+        (27, 31),
+        (32, 35),
+        (36, 40),
+        (40, 42),
+    ]
+
+
+def test_compress_full_files():
+    diskmap = read_diskmap("test_input.txt")
+    blocks = diskmap_to_blocks(diskmap)
+    print("UNCOMPRESSED: " + "".join(blocks))
+    compressed_blocks = compress_full_files(blocks)
+    print("COMPRESSED: " + "".join(compressed_blocks))
+    assert "".join(compressed_blocks) == "00992111777.44.333....5555.6666.....8888.."
+    assert checksum(compressed_blocks) == 2858
