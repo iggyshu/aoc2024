bb19f8dafaede1d94967ca6734c0e387870aaff9
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-14 17:01:44 +0100

    good move, bad rating

--- day14/solution.py
+++ day14/solution.py
@@ -17,13 +17,13 @@ class Robot:
             self.x += self.vel_x
             self.y += self.vel_y
             if self.x >= self.grid_x:
-                self.x = 0
+                self.x -= self.grid_x
             elif self.x < 0:
-                self.x = self.grid_x - 1
+                self.x += self.grid_x
             if self.y >= self.grid_y:
-                self.y = 0
+                self.y -= self.grid_y
             elif self.y < 0:
-                self.y = self.grid_y - 1
+                self.y += self.grid_y
 
     def is_in_quadrant(
         self, quadrant_start: Tuple[int, int], quadrant_end: Tuple[int, int]
@@ -46,6 +46,61 @@ def read_robots(gridsize: Tuple[int, int], filename: str = "input.txt") -> List[
             robots.append(Robot(pos, vel, gridsize))
 
     return robots
+
+
+def print_grid(robots, X, Y):
+    print("==================================")
+    for i in range(Y):
+        counts = []
+        for j in range(X):
+            count = 0
+            for robot in robots:
+                if robot.x == j and robot.y == i:
+                    count += 1
+            counts.append(str(count))
+            if counts[-1] == "0":
+                counts[-1] = "."
+        print(" ".join(counts))
+    print("==================================")
+
+
+def safety_rating(robots: List[Robot], gridsize: Tuple[int, int], seconds: int):
+    X, Y = gridsize
+
+    up_left = (0, 0), (X // 2 - 1, Y // 2 - 1)
+    up_right = (X // 2 + 1, 0), (X - 1, Y // 2 - 1)
+    down_left = (0, Y // 2 + 1), (X // 2 - 4, Y - 1)
+    down_right = (X // 2 + 1, Y // 2 + 1), (X - 1, Y - 1)
+    quadrants = [up_left, up_right, down_left, down_right]
+
+    print_grid(robots, X, Y)
+    for _ in range(seconds):
+        for robot in robots:
+            robot.move(1)
+        print_grid(robots, X, Y)
+
+    print()
+    print("Quadrants:")
+    for q in quadrants:
+        print(q)
+
+    for robot in robots:
+        print(robot)
+
+    robots_quadrants = []
+    for q in quadrants:
+        res = 0
+        for robot in robots:
+            if robot.is_in_quadrant(q[0], q[1]):
+                res += 1
+        robots_quadrants.append(res)
+
+    print("Robots in quadrants:")
+    print(robots_quadrants)
+
+    rating = sum(robots_quadrants)
+    print(f"Safety rating: {rating}")
+    return rating
 
 
 def main():
--- day14/solution_test.py
+++ day14/solution_test.py
@@ -1,4 +1,4 @@
-from solution import read_robots
+from solution import read_robots, safety_rating
 
 
 def test_read_robots():
@@ -6,3 +6,17 @@ def test_read_robots():
     for robot in robots:
         print(robot)
     assert len(robots) == 12
+
+
+def test_safety_rating_one_robot():
+    gridsize = (11, 7)
+    robots = read_robots(gridsize, "test_small_input.txt")
+    rating = safety_rating(robots, gridsize, 5)
+    assert rating == 0
+
+
+def test_safety_rating():
+    gridsize = (11, 7)
+    robots = read_robots(gridsize, "test_input.txt")
+    rating = safety_rating(robots, gridsize, 100)
+    assert rating == 0
