68499495327f1cb5933e4fda2f5017f110dc23ce
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-15 15:06:22 +0100

    day 15, add Solution class

--- day15/solution.py
+++ day15/solution.py
@@ -1,33 +1,94 @@
 from typing import List, Tuple
 
 
-def read_input(filename: str) -> Tuple[List[List[str]], List[str]]:
-    grid = []
-    moves = []
-    with open(filename) as f:
-        lines = f.readlines()
+class Solution:
+    def __init__(self, input_filename: str):
+        grid, moves = self.read_input(input_filename)
+        self._grid = grid
+        self._moves = moves
 
-        i = 0
-        while lines[i].rstrip("\n") != "":
-            grid.append(list(lines[i].rstrip("\n")))
+    def read_input(self, filename: str) -> Tuple[List[List[str]], List[str]]:
+        grid = []
+        moves = []
+        with open(filename) as f:
+            lines = f.readlines()
+
+            i = 0
+            while lines[i].rstrip("\n") != "":
+                grid.append(list(lines[i].rstrip("\n")))
+                i += 1
+
             i += 1
 
-        i += 1
+            commands = ""
+            for j in range(i, len(lines)):
+                commands += lines[j].rstrip("\n")
+            moves = list(commands)
 
-        commands = ""
-        for j in range(i, len(lines)):
-            commands += lines[j].rstrip("\n")
-        moves = list(commands)
+        return grid, moves
 
-    return grid, moves
+    def find_robot(self):
+        for y, row in enumerate(self._grid):
+            for x, cell in enumerate(row):
+                if cell == "@":
+                    return y, x
+        raise Exception("What happened to the robot!?")
 
+    def get_adj_cell(self, coordinates, direction) -> Tuple[str, Tuple[int, int]]:
+        y, x = coordinates
+        match direction:
+            case "<":
+                return self._grid[y][x - 1], (y, x - 1)
+            case ">":
+                return self._grid[y][x + 1], (y, x + 1)
+            case "^":
+                return self._grid[y - 1][x], (y - 1, x)
+            case "v":
+                return self._grid[y + 1][x], (y + 1, x)
+            case _:
+                raise ValueError("Something is wrong with your direction!")
 
-def find_robot(grid: List[List[str]]):
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == "@":
-                return y, x
-    raise Exception("What happened to the robot!?")
+    def swap(self, coord, adj_coord):
+        y, x = coord
+        a_y, a_x = adj_coord
+        self._grid[y][x], self._grid[a_y][a_x] = self._grid[a_y][a_x], self._grid[y][x]
+
+    def move_box(self, box: Tuple[int, int], direction: str) -> bool:
+        adj_value, adj_coord = self.get_adj_cell(box, direction)
+
+        if adj_value == ".":
+            # empty space
+            self.swap(box, adj_coord)
+            return True
+        elif adj_value == "#":
+            return False  # we hit a wall :)
+        elif adj_value == "O":
+            # another box
+            if self.move_box(adj_coord, direction):
+                self.swap(box, adj_coord)
+                return True
+            else:
+                # the other box doesn't move
+                return False
+        else:
+            raise ValueError("Something is wrong with adj cell!")
+
+    def move_robot(self, robot: Tuple[int, int], direction: str):
+        adj_value, adj_coord = self.get_adj_cell(robot, direction)
+
+        if adj_value == ".":
+            self.swap(robot, adj_coord)
+            return adj_coord
+        elif adj_value == "#":
+            return robot
+        elif adj_value == "O":
+            if self.move_box(adj_coord, direction):
+                self.swap(robot, adj_coord)
+                return adj_coord
+            else:
+                return robot
+        else:
+            raise ValueError("Something is wrong with adj cell! - Robot")
 
 
 def main():
--- day15/solution_test.py
+++ day15/solution_test.py
@@ -1,15 +1,16 @@
-from solution import find_robot, read_input
+from solution import Solution
 
 
 def test_read_input():
-    grid, moves = read_input(filename="test_input.txt")
+    solution = Solution(input_filename="test_input.txt")
 
-    assert len(grid) == 10
-    assert len(grid[0]) == 10
-    assert len(moves) == 700
+    assert len(solution._grid) == 10
+    assert len(solution._grid[0]) == 10
+    assert len(solution._moves) == 700
 
 
 def test_find_robot():
-    grid, _ = read_input(filename="test_input.txt")
-    y, x = find_robot(grid)
+    solution = Solution(input_filename="test_input.txt")
+
+    y, x = solution.find_robot()
     assert y == 4 and x == 4
