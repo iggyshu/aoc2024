ff1bd4d8f052a44e24412b02893fa824404a22a9
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-15 15:13:46 +0100

    write a test

--- day15/solution_test.py
+++ day15/solution_test.py
@@ -14,3 +14,30 @@ def test_find_robot():
 
     y, x = solution.find_robot()
     assert y == 4 and x == 4
+
+
+def test_move_robot():
+    solution = Solution(input_filename="test_input.txt")
+    robot = solution.find_robot()
+    for direction in solution._moves:
+        robot = solution.move_robot(robot, direction)
+
+    res = []
+    print()
+    for row in solution._grid:
+        joined = "".join(row)
+        res.append(joined)
+        print(joined)
+
+    assert res == [
+        "##########",
+        "#.O.O.OOO#",
+        "#........#",
+        "#OO......#",
+        "#OO@.....#",
+        "#O#.....O#",
+        "#O.....OO#",
+        "#O.....OO#",
+        "#OO....OO#",
+        "##########",
+    ]
