0891a97da9b8d6d77d58d4c56b8454c20ef738bb
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-16 22:17:22 +0100

    test passes, but need to do a*

--- day16/solution.py
+++ day16/solution.py
@@ -1,11 +1,12 @@
 from typing import List, Tuple
+from collections import deque
 
 
 class Solution:
     def __init__(self, filename: str = "input.txt"):
         self._grid = []
-        self._deer = ()
-        self._end = ()
+        self._deer = (0, 0)
+        self._end = (0, 0)
 
         with open(filename) as f:
             lines = f.readlines()
@@ -26,13 +27,53 @@ class Solution:
             print("".join(row))
 
     def lowest_score(self) -> int:
-        score = 0
+        def is_wall(pos):
+            y, x = pos
+            return self._grid[y][x] == "#"
 
-        return score
+        def is_end(pos):
+            return pos == self._end
+
+        def keep_score(path):
+            if len(path) < 3:
+                return 1
+            y, x = path[-3]
+            y_c, x_c = path[-1]
+
+            if abs(y_c - y) == 1 and abs(x_c - x) == 1:
+                return 1001  # this is a turn
+            else:
+                return 1
+
+        def search(pos, path, score) -> int:
+            score += keep_score(path)
+
+            if is_end(pos):
+                print("REACHED END")
+                print(path)
+                print(score)
+                return score
+            else:
+                y, x = pos
+                left = (y, x - 1)
+                right = (y, x + 1)
+                up = (y - 1, x)
+                down = (y + 1, x)
+
+                scores = [float("inf")]
+
+                for dir in [left, right, up, down]:
+                    if not is_wall(dir) and dir not in path:
+                        scores.append(search(dir, path + [dir], score))
+
+                return min(scores)
+
+        return search(self._deer, [(self._deer[0], self._deer[1] - 1), self._deer], -1)
 
 
 def main():
-    pass
+    solution = Solution(filename="input.txt")
+    print(solution.lowest_score())
 
 
 if __name__ == "__main__":
