069e5c5097ce28632131319c8483855064f385bd
Igors Šutovs <iggy.shu@mykolab.com>
2024-12-28 15:08:36 +0100

    2023: day 3, part 2 correct solution

--- 2023/day3/solution.py
+++ 2023/day3/solution.py
@@ -1,5 +1,5 @@
 from collections import deque
-from typing import Tuple, Union
+from typing import List, Tuple
 from string import punctuation
 
 class Solution():
@@ -49,68 +49,44 @@ class Solution():
             y, x = adj
             if y > 0 and x > 0 and y < len(self._grid) and x < len(self._grid[0]) \
                and self._grid[y][x] in self._symbols:
-                print(f"{self._grid[y][x]} is a symbol")
                 return True
         return False
 
     def sum_of_gear_ratios(self):
         gear_ratios = []
         for gear in self._gears:
-            y, x = gear
-            left = (y, x - 1)
-            right = (y, x + 1)
-            down = (y + 1, x)
-            up = (y - 1, x)
-            up_left = (y - 1, x - 1)
-            up_right = (y - 1, x + 1)
-            down_left = (y + 1, x - 1)
-            down_right = (y + 1, y + 1)
-            adj_numbers = []
+            Y, X = gear
 
-            left_number = self.read_number(left)
-            if left_number is not None:
-                adj_numbers.append(left_number)
+            up = self.read_line((Y - 1, X))
+            curr = self.read_line((Y, X))
+            down = self.read_line((Y + 1, X))
 
-            right_number = self.read_number(right)
-            if right_number is not None:
-                adj_numbers.append(right_number)
+            adj_numbers = up + curr + down
 
-            down_number = self.read_number(down)
-            up_number = self.read_number(up)
-
-            if down_number is not None:
-                adj_numbers.append(down_number)
-            else:
-                down_left_number = self.read_number(down_left)
-                if down_left_number is not None:
-                    adj_numbers.append(down_left_number)
-                down_right_number = self.read_number(down_right)
-                if down_right_number is not None:
-                    adj_numbers.append(down_right_number)
-
-            if up_number is not None:
-                adj_numbers.append(up_number)
-            else:
-                up_left_number = self.read_number(up_left)
-                if up_left_number is not None:
-                    adj_numbers.append(up_left_number)
-                up_right_number = self.read_number(up_right)
-                if up_right_number is not None:
-                    adj_numbers.append(up_right_number)
-
-            print(f"gear ({gear}), adj numbers: {adj_numbers}")
+            print(f"Gear({gear}), adj numbers: {adj_numbers}")
             if len(adj_numbers) == 2:
                 ratio = adj_numbers[0] * adj_numbers[1]
                 gear_ratios.append(ratio)
 
         return sum(gear_ratios)
 
-    def read_number(self, coordinates: Tuple[int, int]) -> Union[int, None]:
+    def read_line(self, coordinates: Tuple[int, int]) -> List[int]:
         Y, X = coordinates
-        if (Y < 0 or Y > len(self._grid)) or (X < 0 or X > len(self._grid[0])):
-            return None
-        if not self._grid[Y][X].isnumeric():
-            return None
+        if Y < 0 or Y > len(self._grid):
+            return []
+
+        numbers = []
+        if self._grid[Y][X].isnumeric():
+            numbers.append(self.read_number(coordinates))
+        else:
+            if X - 1 > 0 and self._grid[Y][X - 1].isnumeric():
+                numbers.append(self.read_number((Y, X - 1)))
+            if X + 1 < len(self._grid[0]) and self._grid[Y][X + 1].isnumeric():
+                numbers.append(self.read_number((Y, X + 1)))
+        return numbers
+
+    def read_number(self, coordinates: Tuple[int, int]) -> int:
+        Y, X = coordinates
         chars = deque([self._grid[Y][X]])
         left = X - 1
         while left >= 0 and self._grid[Y][left].isnumeric():
@@ -120,16 +96,15 @@ class Solution():
         while right < len(self._grid[0]) and self._grid[Y][right].isnumeric():
             chars.append(self._grid[Y][right])
             right += 1
-        print(f"chars: {''.join(chars)}")
         return int("".join(chars))
 
 
 def main():
     sln = Solution()
     part_numbers_sum = sln.sum_of_all_the_part_numbers()
-    print(f"Sum of all engine part numbers: {part_numbers_sum}")
-    gear_ratios_sum = sln.sum_of_all_the_part_numbers()
-    print(f"Sum of gear ratios: {gear_ratios_sum}")
+    print(f"Part 1. Sum of all engine part numbers: {part_numbers_sum}")
+    gear_ratios_sum = sln.sum_of_gear_ratios()
+    print(f"Part 2. Sum of gear ratios: {gear_ratios_sum}")
 
 if __name__ == "__main__":
     main()
